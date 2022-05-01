0cd7ea2dfcaa84773243ba987fad63d20ed12a90
Vitor Oliveira <vitor.oliveira.24.24@gmail.com>
2022-05-01 20:32:14 -0300

    Fix auth tests.

--- second-project/auth/src/routes/__test__/currentUser.test.ts
+++ second-project/auth/src/routes/__test__/currentUser.test.ts
@@ -7,7 +7,7 @@ describe("Test current-user route", () => {
     const endpoint = "/api/v1/users/current-user"
     const user = { email: "vitor@test.com", password: "1234" }
 
-    test("It should return the email and the id of the logged in user", async () => {
+    test("It should return the email of the logged in user", async () => {
         const cookies = await globalThis.signup()
         const response = await agent.get(endpoint).set("Cookie", cookies).expect(200)
         expect(response.body).toHaveProperty("email", user.email)
