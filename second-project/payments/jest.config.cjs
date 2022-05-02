094b935d2b7fec1949d143eb4d4e5a73e82a6e38
Vitor Oliveira <vitor.oliveira.24.24@gmail.com>
2022-05-01 22:31:42 -0300

    Fix payments service test env.

--- second-project/payments/jest.config.cjs
+++ second-project/payments/jest.config.cjs
@@ -1,5 +1,19 @@
 /* eslint-disable no-undef */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { accessSync } = require("fs")
+
+let hasLocalEnv = false
+
+try {
+    accessSync("./src/test/local-test-env.ts")
+    hasLocalEnv = true
+    console.info("Running in local environment")
+} catch (err) {
+    console.warn("Running in external environment")
+}
+
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
@@ -7,8 +21,10 @@ module.exports = {
     setupFilesAfterEnv: [
         "./src/test/setup.ts"
     ],
-    setupFiles: [
-        "./src/test/env.ts"
+    setupFiles: hasLocalEnv ? [
+        "./src/test/local-test-env.ts"
+    ] : [
+        "./src/test/env-without-stripe.ts"
     ],
     injectGlobals: false,
     testTimeout: 60000, // 1 minute
